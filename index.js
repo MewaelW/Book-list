04838c8a00e02f1254730f32493bd0f9e6a4f1d4
Woldegebriel <mamewael@gmail.com>
2023-09-27 12:09:19 +0300

    Adding books to list is done!

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let books = [];
+function showHome() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+    <h1>Welcome to your Book List</h1>
+    <p>This section displays your personal book list</p>
+    <div id="home-section">
+    <h2>Add a Book</h2>
+    <input type="text" id="book-input" placeholder="Enter book name">
+    <button onclick="addBook()">Add Book</button>
+  </div>
+    `;
+
+}
+
+function showBooks() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+    <h1>Book List</h1>
+    <ul class="book-list">
+        ${books.map(book => `<li> ${book}</li>`).join('')}
+    </ul>
+    `
+
+}
+
+function addBook(){
+    const input = document.getElementById('book-input');
+    const bookName = input.value.trim();
+
+    if (bookName !== '') {
+        books.push(bookName);
+        input.value = '';
+        showBooks();
+    }
+}
+showHome();
